48b873ef202cdc4d83d7a2e1bef006bf0309af45
N0WST4NDUP <dlwotjtj9898@gmail.com>
2025-03-03 07:46:49 +0900

    add: update favicon

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './7_app/styles/index.css';
 import App from './5_pages/App';
-import reportWebVitals from './7_app/reportWebVitals';
+import reportWebVitals from './7_app/configs/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
